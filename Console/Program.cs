4f1c7fa234fcd6fe8805a5dd0e632c2f3b18ad5b
Bruno <brunobentocaina@gmail.com>
2024-06-25 18:30:01 -0300

    Adicionar arquivos de projeto.

---
+++ Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseUrls("http://localhost:8080");
+                webBuilder.Configure(app =>
+                {
+                    app.UseRouting();
+
+                    app.UseEndpoints(endpoints =>
+                    {
+                        endpoints.MapGet("/consulta-placa/{placa}", async context =>
+                        {
+                            var placa = context.Request.RouteValues["placa"];
+                            await context.Response.WriteAsync($"Placa recebida: {placa}");
+                        });
+                    });
+                });
+            });
+}
